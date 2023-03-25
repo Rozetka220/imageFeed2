2b98f96345476dbc31456c39913748eab0cd7423
Timur <bikb.timur@yandex.ru>
2023-03-25 19:28:52 +0500

    спринт 8, работает все, кроме корректного заполнения ячеек картинками

---
+++ ImageFeed2/ImagesList/ImagesListCell.swift
@@ -0,0 +1,16 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed2
+//
+//  Created by Аделия Исхакова on 21.03.2023.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    
+    @IBOutlet weak var cellLikeButton: UIButton!
+    @IBOutlet weak var cellDataLabel: UILabel!
+    @IBOutlet weak var cellImageView: UIImageView!
+}
