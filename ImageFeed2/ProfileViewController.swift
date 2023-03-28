c680e96414d30600ca4463b458a79c0c01d3b493
Timur <bikb.timur@yandex.ru>
2023-03-28 20:14:57 +0500

    Добавлен ViewController с профилем пользователя

---
+++ ImageFeed2/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed2
+//
+//  Created by Аделия Исхакова on 27.03.2023.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    @IBOutlet weak var exitButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func clickedExitButton(_ sender: Any) {
+    }
+}
