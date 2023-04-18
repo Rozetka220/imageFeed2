3d6d072523710d760ff462c9f8f2d6a2b94b7a5a
Timur <bikb.timur@yandex.ru>
2023-04-18 23:11:17 +0500

    add model Constant

---
+++ ImageFeed2/Auth/Models/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  ImageFeed2
+//
+//  Created by Аделия Исхакова on 18.04.2023.
+//
+
+import Foundation
+
+struct Constants {
+    let accessKey = "_aGxZoR4YLyuXvZGKijjU6qYtAstJm0xvmNZIUXffmA"
+    let secretKey = "YSz3hJBNjhwhvyr2GfQPGrKeKZsm-6Z6NVU_mRxupLk"
+    let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    let accessScope = "public+read_user+write_likes"
+    let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+}
