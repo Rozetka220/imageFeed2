de8cd76f75b872c3152658fae7bb1f57120c37a1
Timur <bikb.timur@yandex.ru>
2023-06-20 20:13:16 +0500

    тест

---
+++ ImageFeed2Tests/ImagesListServiceTests.swift
@@ -0,0 +1,29 @@
+//
+//  ImageFeed2Tests.swift
+//  ImageFeed2Tests
+//
+//  Created by Аделия Исхакова on 20.06.2023.
+//
+@testable import ImageFeed2
+import XCTest
+
+
+final class ImagesListServiceTests: XCTestCase {
+    func testFetchPhotos() {
+        let service = ImagesListService()
+        
+        let expectation = self.expectation(description: "Wait for Notification")
+        NotificationCenter.default.addObserver(
+            forName: ImagesListService.DidChangeNotification,
+            object: nil,
+            queue: .main) { _ in
+                expectation.fulfill()
+            }
+        
+        service.fetchPhotosNextPage()
+        wait(for: [expectation], timeout: 10)
+        
+        XCTAssertEqual(service.photos.count, 10)
+    }
+}
+
